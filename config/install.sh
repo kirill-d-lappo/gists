35d3acfa910bfa428f5e77287f7179dea7c8215e
Kirill Lappo <kirill-lappo@outlook.com>
2022-12-26 00:28:43 +0300

    added .inputrc

--- config/install.sh
+++ config/install.sh
@@ -4,12 +4,13 @@ wd=$(dirname "$(realpath $0)")
 
 cp -v "$wd/.tmux.conf" -t ~/
 cp -v "$wd/.vimrc"     -t ~/
+cp -v "$wd/.inputrc"   -t ~/
 
 cp -r -v "$wd/.config" -t ~/
 
 
 # append .bashrc instead of rewriting the whole file
-echo "Appending user setups to .bashrc" 
+echo "Appending user setups to .bashrc"
 cat "$wd/.bashrc-append" >> ~/.bashrc
 
 echo "Sourcing ~/.bashrc"
