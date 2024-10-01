6c3d40348d7fc1a4200de0079f6bee3ca1b3ac16
Kirill Lappo <kirill.lappo@digitech7.by>
2024-10-01 19:49:06 +0300

    nvim update

--- config/_config/nvim/lua/klappo/plugins/alpha.lua
+++ config/_config/nvim/lua/klappo/plugins/alpha.lua
@@ -1,53 +1,43 @@
 return {
 	"goolord/alpha-nvim",
+	dependencies = {
+		"ajatkj/vim-qotd",
+		"kirill-d-lappo/nvim-daily-quotes",
+	},
 	event = "VimEnter",
 	config = function()
 		local alpha = require("alpha")
 		local dashboard = require("alpha.themes.dashboard")
 
-		-- Set header
-		-- dashboard.section.header.val = {
-		-- 	"                                                     ",
-		-- 	"  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
-		-- 	"  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
-		-- 	"  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
-		-- 	"  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
-		-- 	"  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
-		-- 	"  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
-		-- 	"                                                     ",
-		-- }
+		local logos = require("nvim-logos")
 
-		-- dashboard.section.header.val = {
-		-- 	" ::::    ::: :::::::::: ::::::::  :::     ::: ::::::::::: ::::    ::::  ",
-		-- 	" :+:+:   :+: :+:       :+:    :+: :+:     :+:     :+:     +:+:+: :+:+:+ ",
-		-- 	" :+:+:+  +:+ +:+       +:+    +:+ +:+     +:+     +:+     +:+ +:+:+ +:+ ",
-		-- 	" +#+ +:+ +#+ +#++:++#  +#+    +:+ +#+     +:+     +#+     +#+  +:+  +#+ ",
-		-- 	" +#+  +#+#+# +#+       +#+    +#+  +#+   +#+      +#+     +#+       +#+ ",
-		-- 	" #+#   #+#+# #+#       #+#    #+#   #+#+#+#       #+#     #+#       #+# ",
-		-- 	" ###    #### ########## ########      ###     ########### ###       ### ",
-		-- }
-
-		dashboard.section.header.val = {
-			"                                                                                ",
-			"  ██████   █████ ██████████    ███████    █████   █████ █████ ██████   ██████   ",
-			"  ░░██████ ░░███ ░░███░░░░░█  ███░░░░░███ ░░███   ░░███ ░░███ ░░██████ ██████   ",
-			"   ░███░███ ░███  ░███  █ ░  ███     ░░███ ░███    ░███  ░███  ░███░█████░███   ",
-			"   ░███░░███░███  ░██████   ░███      ░███ ░███    ░███  ░███  ░███░░███ ░███   ",
-			"   ░███ ░░██████  ░███░░█   ░███      ░███ ░░███   ███   ░███  ░███ ░░░  ░███   ",
-			"   ░███  ░░█████  ░███ ░   █░░███     ███   ░░░█████░    ░███  ░███      ░███   ",
-			"   █████  ░░█████ ██████████ ░░░███████░      ░░███      █████ █████     █████  ",
-			"  ░░░░░    ░░░░░ ░░░░░░░░░░    ░░░░░░░         ░░░      ░░░░░ ░░░░░     ░░░░░   ",
-			"                                                                                ",
-		}
+		-- Set header with logo
+		local get_logo = logos.get_random_logo_func()
+		dashboard.section.header.val = get_logo()
 
 		-- Set menu
 		dashboard.section.buttons.val = {
 			dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
-			dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
+			dashboard.button("F3", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
 			dashboard.button("SPC ff", "󰱼  > Find File", "<cmd>Telescope find_files<CR>"),
-			dashboard.button("SPC fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
-			dashboard.button("SPC wr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
-			dashboard.button("q", "  > Quit NVIM", "<cmd>qa<CR>"),
+			dashboard.button("SPC fs", "  > Find String", "<cmd>Telescope live_grep<CR>"),
+			dashboard.button("SPC wr", "󰁯  > Work Restore", "<cmd>SessionRestore<CR>"),
+			dashboard.button("q", "  > Quit", "<cmd>qa<CR>"),
+		}
+
+		local dq = require("nvim-daily-quotes")
+		local quote = dq.get_qod()
+
+		local padding_length = 3
+		local border_length = #quote + padding_length * 2
+		local border = string.rep("=", border_length)
+		local padding = string.rep(" ", padding_length)
+		local text = padding .. quote .. padding
+
+		dashboard.section.footer.val = {
+			border,
+			text,
+			border,
 		}
 
 		-- Send config to alpha
