72336c63ac471869c77893648651bc1f9a3574e0
Kirill Lappo <kiril-lappo@outlook.com>
2024-10-19 21:46:50 +0300

    sync.ps1 fixed

--- config/_config/nvim/sync.ps1
+++ config/_config/nvim/sync.ps1
@@ -1,10 +1,11 @@
 # rsync --archive --progress --exclude ":- /home/klappo/.config/nvim/.gitignore" "/home/klappo/.config/nvim/" "/home/klappo/workspace/remote/gists/config/_config/nvim/"
 
 # Install from repo to config folder
-[Parameter(AttributeValues)]
+
+param (
 [switch]
 $Install
-
+)
 $configFolderPath = "$env:USERPROFILE/AppData/Local/nvim"
 $repoFolderPath = "$env:USERPROFILE/w/remote/gists/config/_config/nvim"
 
@@ -12,6 +13,6 @@ if ($Install) {
 Copy-Item -Recurse -Force -Path "$repoFolderPath/*" -Destination "$configFolderPath"
 }
 else {
-Copy-Item -Recurse -Force -Path "$configFolderPath/*" -Destination "$repoFolderPath"
+ Copy-Item -Recurse -Force -Path "$configFolderPath/*" -Destination "$repoFolderPath"
 }
 
