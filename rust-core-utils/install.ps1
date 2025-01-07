936a4223c16537cb65cb34c4de6270388c4c4822
Kirill Lappo <kirill.lappo@digitech7.by>
2025-01-07 20:27:55 +0300

    ls: default params update for eza

--- rust-core-utils/install.ps1
+++ rust-core-utils/install.ps1
@@ -3,51 +3,51 @@
 
 [CmdletBinding()]
 param (
-  # Target path where binaries and bat files will be installed to.
-  # The path must be a part of your PATH vairable.
-  [Parameter()]
-  [Alias("t")]
-  [Alias("target")]
-  [string]
-  $TargetFolderPath = "$env:USERPROFILE\.local\bin",
+	# Target path where binaries and bat files will be installed to.
+	# The path must be a part of your PATH vairable.
+	[Parameter()]
+	[Alias("t")]
+	[Alias("target")]
+	[string]
+	$TargetFolderPath = "$env:USERPROFILE\.local\bin",
 
-  # Local path to Rust Core Utils source code.
-  # GitHub: https://github.com/uutils/coreutils
-  [Parameter()]
-  [Alias("s")]
-  [Alias("source")]
-  [string]
-  # $SourcesPath = "C:\Users\klappo\workspace\remote\coreutils",
-  $SourcesPath,
+	# Local path to Rust Core Utils source code.
+	# GitHub: https://github.com/uutils/coreutils
+	[Parameter()]
+	[Alias("s")]
+	[Alias("source")]
+	[string]
+	# $SourcesPath = "C:\Users\klappo\workspace\remote\coreutils",
+	$SourcesPath,
 
-  # Skip build step
-  [Parameter()]
-  [Alias("no-build")]
-  [switch]
-  $NoBuild = $False,
+	# Skip build step
+	[Parameter()]
+	[Alias("no-build")]
+	[switch]
+	$NoBuild = $False,
 
-  # Do not generate powershel completion file
-  [Parameter()]
-  [Alias("no-completion")]
-  [switch]
-  $NoAutocompletionRegen = $False,
+	# Do not generate powershel completion file
+	[Parameter()]
+	[Alias("no-completion")]
+	[switch]
+	$NoAutocompletionRegen = $False,
 
-  # Do not generate bat files
-  [Parameter()]
-  [Alias("no-bat")]
-  [switch]
-  $NoBatRegen = $False,
+	# Do not generate bat files
+	[Parameter()]
+	[Alias("no-bat")]
+	[switch]
+	$NoBatRegen = $False,
 
-  # Do not wait, start immediatly
-  [Parameter()]
-  [Alias("no-wait")]
-  [switch]
-  $NoWait = $False,
+	# Do not wait, start immediatly
+	[Parameter()]
+	[Alias("no-wait")]
+	[switch]
+	$NoWait = $False,
 
-  [Parameter()]
-  [Alias("clean-src")]
-  [switch]
-  $CleanSources = $False
+	[Parameter()]
+	[Alias("clean-src")]
+	[switch]
+	$CleanSources = $False
 )
 
 #region 3d party scripts
@@ -55,8 +55,9 @@ param (
 #https://www.powershellgallery.com/packages/start-countdowntimer/1.0.1
 # -----------------------  - by stib from powershellgallery.com ------------------------------
 # ----------------------------------countdown timer-------------------------------------------
-function Start-CountdownTimer {
-  <#
+function Start-CountdownTimer
+{
+	<#
 
   .SYNOPSIS
   Displays a text-based countdown timer in the console.
@@ -88,63 +89,67 @@ function Start-CountdownTimer {
   http://blob.pureandapplied.com.au/?p=875
 
   #>
-  param (
-    [string]$Text,
-    [int]$Days = 0,
-    [int]$Hours = 0,
-    [int]$Minutes = 0,
-    [int]$Seconds = 0,
-    [int]$TickLength = 50
-  )
+	param (
+		[string]$Text,
+		[int]$Days = 0,
+		[int]$Hours = 0,
+		[int]$Minutes = 0,
+		[int]$Seconds = 0,
+		[int]$TickLength = 50
+	)
 
-  function Format-Time {
-    param (
-      [timespan]$Time
-    )
+	function Format-Time
+	{
+		param (
+			[timespan]$Time
+		)
 
-    "$($Time.ToString("ss")) seconds"
-  }
+		"$($Time.ToString("ss")) seconds"
+	}
 
-  $waitTime = New-TimeSpan -Days $Days -Hours $Hours -Minutes $Minutes -Seconds $Seconds
-  # $spinner = @( '⠁', '⠂', '⠄', '⡀', '⢀', '⠠', '⠐', '⠈' )
-  # $spinner = @( '⣷', '⣯', '⣟', '⡿', '⢿', '⣻', '⣽', '⣾' )
-  # $spinner = @( '⠉', '⠈', '⠘' , '⠐', '⠰', '⠠' , '⠤', '⠄', '⠆', '⠂', '⠃', '⠁')
-  $spinner = @( '⠉', '⠙', '⠘' , '⠸', '⠰', '⠴' , '⠤', '⠦', '⠆', '⠇', '⠃', '⠋')
-  $completedChar = "✓";
+	$waitTime = New-TimeSpan -Days $Days -Hours $Hours -Minutes $Minutes -Seconds $Seconds
+	# $spinner = @( '⠁', '⠂', '⠄', '⡀', '⢀', '⠠', '⠐', '⠈' )
+	# $spinner = @( '⣷', '⣯', '⣟', '⡿', '⢿', '⣻', '⣽', '⣾' )
+	# $spinner = @( '⠉', '⠈', '⠘' , '⠐', '⠰', '⠠' , '⠤', '⠄', '⠆', '⠂', '⠃', '⠁')
+	$spinner = @( '⠉', '⠙', '⠘' , '⠸', '⠰', '⠴' , '⠤', '⠦', '⠆', '⠇', '⠃', '⠋')
+	$completedChar = "✓";
 
-  $spinnerPos = 0
-  $spinnerLen = $spinner.Length
+	$spinnerPos = 0
+	$spinnerLen = $spinner.Length
 
-  $targetTime = (get-date) + $waitTime
-  $remain = ($targetTime - (get-date))
-  if ($Text) {
-    $formattedText = "$Text in "
-  }
+	$targetTime = (get-date) + $waitTime
+	$remain = ($targetTime - (get-date))
+	if ($Text)
+	{
+		$formattedText = "$Text in "
+	}
 
-  $origpos = $host.UI.RawUI.CursorPosition
+	$origpos = $host.UI.RawUI.CursorPosition
 
-  try {
-    while ($remain.TotalSeconds -gt 0) {
-      $formattedRemain = Format-Time $remain
+	try
+	{
+		while ($remain.TotalSeconds -gt 0)
+		{
+			$formattedRemain = Format-Time $remain
 
-      Write-Host (" {0} " -f $spinner[$spinnerPos % $spinnerLen]) -NoNewline
-      write-host (" {0}{1}" -f $formattedText, $formattedRemain) -NoNewline
-      Write-Host "`r" -NoNewline
+			Write-Host (" {0} " -f $spinner[$spinnerPos % $spinnerLen]) -NoNewline
+			write-host (" {0}{1}" -f $formattedText, $formattedRemain) -NoNewline
+			Write-Host "`r" -NoNewline
 
-      $spinnerPos += 1
-      Start-Sleep -Milliseconds $TickLength
-      $remain = ($targetTime - (get-date))
-    }
-  }
-  finally {
-    $host.UI.RawUI.CursorPosition = $origpos
-  }
+			$spinnerPos += 1
+			Start-Sleep -Milliseconds $TickLength
+			$remain = ($targetTime - (get-date))
+		}
+	} finally
+	{
+		$host.UI.RawUI.CursorPosition = $origpos
+	}
 
-  $terminalWidth = $Host.UI.RawUI.WindowSize.Width
-  Write-Host "$(' ' * $terminalWidth)`r" -NoNewline
+	$terminalWidth = $Host.UI.RawUI.WindowSize.Width
+	Write-Host "$(' ' * $terminalWidth)`r" -NoNewline
 
-  Write-Host " $completedChar " -NoNewline
-  Write-Host " $Text now!"
+	Write-Host " $completedChar " -NoNewline
+	Write-Host " $Text now!"
 }
 
 #endregion 3d part scripts
@@ -153,9 +158,10 @@ function Start-CountdownTimer {
 # Powershell Hall of Shame
 $InformationPreference = "Continue"
 
-if (-Not (Test-Path $TargetFolderPath)) {
-  Write-Information "Missing folder, it will be created: '$TargetFolderPath'"
-  New-Item $TargetFolderPath -ItemType Directory
+if (-Not (Test-Path $TargetFolderPath))
+{
+	Write-Information "Missing folder, it will be created: '$TargetFolderPath'"
+	New-Item $TargetFolderPath -ItemType Directory
 }
 
 $profileRoot = Split-Path "$PROFILE" -Parent
@@ -166,210 +172,220 @@ $EntryPointFileName = "PowerShell.CoreUtils.EntryPoint.ps1"
 $EntryPointFilePath = "$profileRoot\$EntryPointFileName"
 
 $CargoUtilsSourcePath = $SourcesPath
-function Get-ManifestPath {
-  "$CargoUtilsSourcePath\Cargo.toml"
+function Get-ManifestPath
+{
+	"$CargoUtilsSourcePath\Cargo.toml"
 }
 
 $coreutils = @(
-  "["
-  "b2sum"
-  "b3sum"
-  "base32"
-  "base64"
-  "basename"
-  "basenc"
-  "cat"
-  "cksum"
-  "comm"
-  "cp"
-  "csplit"
-  "cut"
-  "date"
-  "dd"
-  "df"
-  "dir"
-  "dircolors"
-  "dirname"
-  "du"
-  "echo"
-  "env"
-  "expand"
-  "expr"
-  "factor"
-  "false"
-  "fmt"
-  "fold"
-  "hashsum"
-  "head"
-  "join"
-  "link"
-  "ln"
-  "ls"
-  "md5sum"
-  "mkdir"
-  "mktemp"
-  "more"
-  "mv"
-  "nl"
-  "numfmt"
-  "od"
-  "paste"
-  "pr"
-  "printenv"
-  "printf"
-  "ptx"
-  "pwd"
-  "readlink"
-  "realpath"
-  "rm"
-  "rmdir"
-  "seq"
-  "sha1sum"
-  "sha224sum"
-  "sha256sum"
-  "sha3-224sum"
-  "sha3-256sum"
-  "sha3-384sum"
-  "sha3-512sum"
-  "sha384sum"
-  "sha3sum"
-  "sha512sum"
-  "shake128sum"
-  "shake256sum"
-  "shred"
-  "shuf"
-  "sleep"
-  "sort"
-  "split"
-  "sum"
-  "tac"
-  "tail"
-  "tee"
-  "test"
-  "touch"
-  "tr"
-  "true"
-  "truncate"
-  "tsort"
-  "unexpand"
-  "uniq"
-  "unlink"
-  "vdir"
-  "wc"
-  "yes"
+	"["
+	"b2sum"
+	"b3sum"
+	"base32"
+	"base64"
+	"basename"
+	"basenc"
+	"cat"
+	"cksum"
+	"comm"
+	"cp"
+	"csplit"
+	"cut"
+	"date"
+	"dd"
+	"df"
+	"dir"
+	"dircolors"
+	"dirname"
+	"du"
+	"echo"
+	"env"
+	"expand"
+	"expr"
+	"factor"
+	"false"
+	"fmt"
+	"fold"
+	"hashsum"
+	"head"
+	"join"
+	"link"
+	"ln"
+	"ls"
+	"md5sum"
+	"mkdir"
+	"mktemp"
+	"more"
+	"mv"
+	"nl"
+	"numfmt"
+	"od"
+	"paste"
+	"pr"
+	"printenv"
+	"printf"
+	"ptx"
+	"pwd"
+	"readlink"
+	"realpath"
+	"rm"
+	"rmdir"
+	"seq"
+	"sha1sum"
+	"sha224sum"
+	"sha256sum"
+	"sha3-224sum"
+	"sha3-256sum"
+	"sha3-384sum"
+	"sha3-512sum"
+	"sha384sum"
+	"sha3sum"
+	"sha512sum"
+	"shake128sum"
+	"shake256sum"
+	"shred"
+	"shuf"
+	"sleep"
+	"sort"
+	"split"
+	"sum"
+	"tac"
+	"tail"
+	"tee"
+	"test"
+	"touch"
+	"tr"
+	"true"
+	"truncate"
+	"tsort"
+	"unexpand"
+	"uniq"
+	"unlink"
+	"vdir"
+	"wc"
+	"yes"
 )
 
 # when you want to override some default utils with alternatives or specify default arguments
 $utilOverrides = @{
-  "ls"  = @{
-    "command" = "eza"
-    "args"    = "--color=auto --icons=auto"
-  }
-  "cat" = @{
-    "command" = "bat"
-  }
+	"ls"  = @{
+		"command" = "eza"
+		"args"    = "--color=auto --icons=auto --group-directories-first -w 1"
+	}
+	"cat" = @{
+		"command" = "bat"
+	}
 };
 
-function Set-EscapeCharacters {
-  Param(
-    [parameter(Mandatory = $true, Position = 0)]
-    [String]
-    $string
-  )
-  $string = $string -replace '\*', '`*'
-  $string = $string -replace '\\', '`\'
-  $string = $string -replace '\~', '`~'
-  $string = $string -replace '\;', '`;'
-  $string = $string -replace '\(', '`('
-  $string = $string -replace '\%', '`%'
-  $string = $string -replace '\?', '`?'
-  $string = $string -replace '\.', '`.'
-  $string = $string -replace '\:', '`:'
-  $string = $string -replace '\@', '`@'
-  $string = $string -replace '\/', '`/'
-  $string = $string -replace '\[', '`['
-  $string = $string -replace '\]', '`]'
-  $string
+function Set-EscapeCharacters
+{
+	Param(
+		[parameter(Mandatory = $true, Position = 0)]
+		[String]
+		$string
+	)
+	$string = $string -replace '\*', '`*'
+	$string = $string -replace '\\', '`\'
+	$string = $string -replace '\~', '`~'
+	$string = $string -replace '\;', '`;'
+	$string = $string -replace '\(', '`('
+	$string = $string -replace '\%', '`%'
+	$string = $string -replace '\?', '`?'
+	$string = $string -replace '\.', '`.'
+	$string = $string -replace '\:', '`:'
+	$string = $string -replace '\@', '`@'
+	$string = $string -replace '\/', '`/'
+	$string = $string -replace '\[', '`['
+	$string = $string -replace '\]', '`]'
+	$string
 }
 
-function Generate-RustCoreUtilsBats() {
+function Generate-RustCoreUtilsBats()
+{
 
-  $coreutils | ForEach-Object {
-    $utilName = $_
+	$coreutils | ForEach-Object {
+		$utilName = $_
 
-    $utilOverride = $utilOverrides[$_]
+		$utilOverride = $utilOverrides[$_]
 
-    $utilCommand = ""
-    $utilArgs = ""
+		$utilCommand = ""
+		$utilArgs = ""
 
-    if ($utilOverride) {
-      $utilCommand = $utilOverride.command
-      $utilArgs = $utilOverride.args
-    }
+		if ($utilOverride)
+		{
+			$utilCommand = $utilOverride.command
+			$utilArgs = $utilOverride.args
+		}
 
-    if (-Not($utilCommand)) {
-      $utilCommand = "`"coreutils.exe`" $utilName"
-    }
+		if (-Not($utilCommand))
+		{
+			$utilCommand = "`"coreutils.exe`" $utilName"
+		}
 
-    $script = @"
+		$script = @"
 @echo off
 $utilCommand $utilArgs %*
 "@
 
 
-    $scriptName = Set-EscapeCharacters $utilName
-    $scriptPath = "$TargetFolderPath/$scriptName.bat"
-    $script > "$scriptPath"
-  }
+		$scriptName = Set-EscapeCharacters $utilName
+		$scriptPath = "$TargetFolderPath/$scriptName.bat"
+		$script > "$scriptPath"
+	}
 }
 
-function Generate-RustCoreUtilsAutocompletion() {
-  $imports = @"
+function Generate-RustCoreUtilsAutocompletion()
+{
+	$imports = @"
 # Generated at: $(Get-Date -Format "yyyy-MM-dd HH:mm:ss")
 using namespace System.Management.Automation
 using namespace System.Management.Automation.Language
 
 "@
 
-  $imports > $AutocompletionCoreUtilsFilePath
+	$imports > $AutocompletionCoreUtilsFilePath
 
-  $autocompletions = @();
-  $coreutils | ForEach-Object {
-    $tool = $_
-    $script = $(cargo run --release --manifest-path "$(Get-ManifestPath)" completion $tool powershell)
-    $script = $script -replace "^using namespace.*$", ""
+	$autocompletions = @();
+	$coreutils | ForEach-Object {
+		$tool = $_
+		$script = $(cargo run --release --manifest-path "$(Get-ManifestPath)" completion $tool powershell)
+		$script = $script -replace "^using namespace.*$", ""
 
-    $autocompletions += $script
+		$autocompletions += $script
 
-  }
+	}
 
-  $autocompletions >> $AutocompletionCoreUtilsFilePath
+	$autocompletions >> $AutocompletionCoreUtilsFilePath
 }
 
-function Generate-AliasRemovalScript {
-  $scriptText = ""
+function Generate-AliasRemovalScript
+{
+	$scriptText = ""
 
-  $scriptText > $AliasRemovalFilePath
+	$scriptText > $AliasRemovalFilePath
 
-  $coreutils | ForEach-Object {
-    $scriptText += "Remove-Alias `"$($_)`" -Force -ErrorAction SilentlyContinue -Scope Global `n"
-  }
+	$coreutils | ForEach-Object {
+		$scriptText += "Remove-Alias `"$($_)`" -Force -ErrorAction SilentlyContinue -Scope Global `n"
+	}
 
-  $scriptText >> $AliasRemovalFilePath
+	$scriptText >> $AliasRemovalFilePath
 }
 
-function Build-InstallRustUtils {
-  cargo build --release --manifest-path "$(Get-ManifestPath)" --locked
+function Build-InstallRustUtils
+{
+	cargo build --release --manifest-path "$(Get-ManifestPath)" --locked
 
-  Copy-Item -Path "$CargoUtilsSourcePath\target\release\coreutils.exe" -Destination "$TargetFolderPath\coreutils.exe"
+	Copy-Item -Path "$CargoUtilsSourcePath\target\release\coreutils.exe" -Destination "$TargetFolderPath\coreutils.exe"
 }
 
-function Update-CoreUtilsEntrypoint {
-  if (-Not (Test-Path "$EntryPointFilePath")) {
-    New-Item "$EntryPointFilePath" -ItemType File -Force
-  }
+function Update-CoreUtilsEntrypoint
+{
+	if (-Not (Test-Path "$EntryPointFilePath"))
+	{
+		New-Item "$EntryPointFilePath" -ItemType File -Force
+	}
 
-  $entryPointLines = @"
+	$entryPointLines = @"
 #region RustCoreUtils
 # Generated at: $(Get-Date -Format "yyyy-MM-dd HH:mm:ss")
 
@@ -384,93 +400,107 @@ $(if (Test-Path "$profileRoot\PowerShell.AutoCompletion.CoreUtils.ps1") { '. "$p
 #endregion RustCoreUtils
 "@
 
-  $entryPointLines > $EntryPointFilePath
+	$entryPointLines > $EntryPointFilePath
 }
 
-function Test-PathVariable {
-  $pathValue = $env:Path
+function Test-PathVariable
+{
+	$pathValue = $env:Path
 
-  if (-Not($pathValue -like "*$TargetFolderPath*")) {
-    Write-Warning "Can't find '$TargetFolderPath' in PATH variable. Make sure it is added to PATH."
-  }
+	if (-Not($pathValue -like "*$TargetFolderPath*"))
+	{
+		Write-Warning "Can't find '$TargetFolderPath' in PATH variable. Make sure it is added to PATH."
+	}
 }
 
-function Clone-Utils {
+function Clone-Utils
+{
 
-  $srcUrl = "https://github.com/uutils/coreutils.git"
-  $localPath = "$PSScriptRoot/_src/coreutils"
+	$srcUrl = "https://github.com/uutils/coreutils.git"
+	$localPath = "$PSScriptRoot/_src/coreutils"
 
-  if (!$localPath -or -Not(Test-Path($localPath)) -or -Not(Test-Path("$localPath/*"))) {
-    New-Item -Path $localPath -ItemType Directory -Force -ErrorAction SilentlyContinue
+	if (!$localPath -or -Not(Test-Path($localPath)) -or -Not(Test-Path("$localPath/*")))
+	{
+		New-Item -Path $localPath -ItemType Directory -Force -ErrorAction SilentlyContinue
 
-    & git clone "$srcUrl" "$localPath"
-  }
+		& git clone "$srcUrl" "$localPath"
+	}
 
 
-  $localPath
+	$localPath
 }
 
-function Ensure-Sources {
-  if (!$script:CargoUtilsSourcePath) {
-    $script:CargoUtilsSourcePath = $(Clone-Utils)
-  }
+function Ensure-Sources
+{
+	if (!$script:CargoUtilsSourcePath)
+	{
+		$script:CargoUtilsSourcePath = $(Clone-Utils)
+	}
 
-  Write-Information "Sources location: '$CargoUtilsSourcePath'"
+	Write-Information "Sources location: '$CargoUtilsSourcePath'"
 
-  if (-Not(Test-Path($CargoUtilsSourcePath))) {
-    Write-Error "Source folder '$CargoUtilsSourcePath' does not exist!"
-    exit 1
-  }
+	if (-Not(Test-Path($CargoUtilsSourcePath)))
+	{
+		Write-Error "Source folder '$CargoUtilsSourcePath' does not exist!"
+		exit 1
+	}
 }
 
-function Clean-All {
+function Clean-All
+{
 
-  Remove-Item $CargoUtilsSourcePath -Force -Recurse
+	Remove-Item $CargoUtilsSourcePath -Force -Recurse
 }
 
-function Generate-RustCoreUtils() {
+function Generate-RustCoreUtils()
+{
 
-  Write-Information "Building and installing Rust Core Utils by uutils teams"
-  Write-Information "Project: https://github.com/uutils/coreutils"
+	Write-Information "Building and installing Rust Core Utils by uutils teams"
+	Write-Information "Project: https://github.com/uutils/coreutils"
 
-  if (! $NoWait) {
-    Start-CountdownTimer -Text "Installing" -Seconds 5
-  }
+	if (! $NoWait)
+	{
+		Start-CountdownTimer -Text "Installing" -Seconds 5
+	}
 
-  Ensure-Sources
+	Ensure-Sources
 
-  if (! $NoBuild) {
-    Write-Information "Building and installing Rust Core Utils..."
-    Build-InstallRustUtils
-  }
+	if (! $NoBuild)
+	{
+		Write-Information "Building and installing Rust Core Utils..."
+		Build-InstallRustUtils
+	}
 
-  if (! $NoBatRegen) {
-    Write-Information "Generating bat files per each command..."
-    Generate-RustCoreUtilsBats
-  }
+	if (! $NoBatRegen)
+	{
+		Write-Information "Generating bat files per each command..."
+		Generate-RustCoreUtilsBats
+	}
 
-  if (! $NoAutocompletionRegen) {
-    Write-Information "Generating powershell autocompletion file for each command..."
-    Generate-RustCoreUtilsAutocompletion
-  }
+	if (! $NoAutocompletionRegen)
+	{
+		Write-Information "Generating powershell autocompletion file for each command..."
+		Generate-RustCoreUtilsAutocompletion
+	}
 
-  Write-Information "Generating Alias removal script..."
-  Generate-AliasRemovalScript
+	Write-Information "Generating Alias removal script..."
+	Generate-AliasRemovalScript
 
-  Update-CoreUtilsEntrypoint
+	Update-CoreUtilsEntrypoint
 
-  Test-PathVariable
+	Test-PathVariable
 
-  if ($CleanSources) {
-    Clean-All
-  }
+	if ($CleanSources)
+	{
+		Clean-All
+	}
 
-  Write-Information "Done."
+	Write-Information "Done."
 
-  Write-Information "Please, add the next line to your `$PROFILE: '$PROFILE'"
-  Write-Information ""
-  Write-Information ". `$PSScriptRoot\$EntryPointFileName"
-  Write-Information ""
+	Write-Information "Please, add the next line to your `$PROFILE: '$PROFILE'"
+	Write-Information ""
+	Write-Information ". `$PSScriptRoot\$EntryPointFileName"
+	Write-Information ""
 }
 
 
